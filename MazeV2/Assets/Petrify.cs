ba46ec8c2f6027ab83bf03813aaaf5512f3cda9f
Savannah Mann <mannnnnn@users.noreply.github.com>
2018-10-25 17:40:34 -0500

    Handy Update & Raycasting Recommendation
    Started building the script for testing petrification via light, but seems like this is something better done with raycasting than collisions as it isn't working atm. Also fixed up the hands to look less weird.

---
+++ MazeV2/Assets/Petrify.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+///I get the feeling this is going to have to be entirely raycasting
+///Which makes sense but still, feels bad man
+/// 5:38pm 10/25/18
+
+
+public class Petrify : MonoBehaviour {
+    public GameObject playerL;
+    public GameObject playerR;
+    public TextMesh uiPiece;
+    bool petrifyOn = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if(petrifyOn){uiPiece.text = "Uh oh.";}
+	}
+
+
+	void OnCollisionEnter(Collision collision){
+     if (collision.gameObject.name == playerL.name || collision.gameObject.name == playerR.name){
+        petrifyOn = true;
+     }
+ }
+
+}
