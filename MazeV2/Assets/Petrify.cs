3de72404886f9a572f08c41407ec354439edefa0
Savannah Mann <sv.bluecow@gmail.com>
2018-10-30 10:42:21 -0500

    Petrify Updates
    Raycasting technique was tried, but was too small to be effective.
    Instead, checking collision with an invisible object made to be the same
    area as the light. Still in progress, non-functional.

--- MazeV2/Assets/Petrify.cs
+++ MazeV2/Assets/Petrify.cs
@@ -2,32 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-///I get the feeling this is going to have to be entirely raycasting
-///Which makes sense but still, feels bad man
-/// 5:38pm 10/25/18
-
-
 public class Petrify : MonoBehaviour {
+
+    public GameObject source;
     public GameObject playerL;
     public GameObject playerR;
     public TextMesh uiPiece;
-    bool petrifyOn = false;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        source.GetComponent<Renderer>().enabled = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	if(petrifyOn){uiPiece.text = "Uh oh.";}
-	}
-
-
-	void OnCollisionEnter(Collision collision){
-     if (collision.gameObject.name == playerL.name || collision.gameObject.name == playerR.name){
-        petrifyOn = true;
-     }
+        
+        
  }
+
+    void OnCollisionEnter(Collision col)
+    {
+        
+        if (col.gameObject.name == playerL.name || col.gameObject.name == playerR.name)
+        {
+            Debug.Log(col.gameObject.name);
+            uiPiece.text = "Oh dear.";
+        }
+        else
+        {
+            //TODO: move to exit
+            uiPiece.text = "All good.";
+        }
+    }
+
+
 
 }
