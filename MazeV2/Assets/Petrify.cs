7d6a10721cc453ab1cf0991c251310b1df929c8d
Savannah Mann <sv.bluecow@gmail.com>
2018-11-01 10:44:14 -0500

    Petrify Invisible Collider
    Triggers weren't working for some reason (my guess is due to how the VR
    controller rigidbodies are being generated) so instead there's a
    collision and then further collisions are ignored. Not the best
    solution, but it's a start. Only works for about a millisecond

--- MazeV2/Assets/Petrify.cs
+++ MazeV2/Assets/Petrify.cs
@@ -2,38 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//https://forum.unity.com/threads/how-can-you-detect-but-still-physically-ignore-collisions.111583/
+//Collider -> Trigger fix by Eric5h5 on the above link
+
+
 public class Petrify : MonoBehaviour {
 
     public GameObject source;
     public GameObject playerL;
     public GameObject playerR;
     public TextMesh uiPiece;
+    private Vector3 lastPosition = new Vector3();
+    private Vector3 lastVelocity = new Vector3();
+    private Vector3 lastAngularVelocity = new Vector3();
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         source.GetComponent<Renderer>().enabled = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        
-        
- }
+        lastPosition = transform.position;
+        lastVelocity = GetComponent<Rigidbody>().velocity;
+        lastAngularVelocity = GetComponent<Rigidbody>().angularVelocity;
 
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
 
     void OnCollisionEnter(Collision col)
     {
+
+        transform.position = lastPosition;
+        GetComponent<Rigidbody>().velocity = lastVelocity;
+        GetComponent<Rigidbody>().angularVelocity = lastAngularVelocity;
+        Physics.IgnoreCollision(GetComponent<Collider>(), col.collider);
+
         Debug.Log(col.gameObject.name);
         if (col.gameObject.name == "LeftControllerScriptAlias" || col.gameObject.name == "RightControllerScriptAlias" || col.gameObject.name == "VRSimulator_CameraRig")
         {
 
             uiPiece.text = "Oh dear.";
         }
-        else
-        {
+
+    }
+
+    void OnCollisionExit(Collision col)
+    {
             //TODO: move to exit
             uiPiece.text = "All good.";
-        }
     }
 
 }
