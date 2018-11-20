1f86d058f9c082475a2f4e0aa38b6c1c18d61c44
Savannah Mann <sv.bluecow@gmail.com>
2018-11-20 10:37:33 -0600

    Animation Controller Testing
    Jenny's Animation can now be triggered with the mouse (no exit conditions)
    Petrify appears to be malfunctioning, looking into fixing it and polishing the code
    TV asset will be added soon

---
+++ MazeV2/Assets/animationSwitcher.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationSwitcher : MonoBehaviour {
+
+    public Animator jennyAnimation;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Animation 'hurt' should be playing.");
+            jennyAnimation.SetBool("jennyHurt", true);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Animation 'hurt' should be playing.");
+            jennyAnimation.SetBool("jennyAttack", true);
+        }
+
+        if (Input.GetMouseButtonDown(2))
+        {
+            Debug.Log("Animation 'hurt' should be playing.");
+            jennyAnimation.SetBool("jennyHide", true);
+        }
+
+
+
+    }
+}
