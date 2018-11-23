d342adef14995007f2356567c17f461d2c4dddaa
Savannah Mann <sv.bluecow@gmail.com>
2018-11-23 14:05:47 -0600

    Revert "Working on Setting Up Animation Transitions"
    This reverts commit 726103e88eadeab9e08f1641477f0b2f4f70502f.

--- MazeV2/Assets/Petrify.cs
+++ MazeV2/Assets/Petrify.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//https://forum.unity.com/threads/how-can-you-detect-but-still-physically-ignore-collisions.111583/
+//Collider -> Trigger fix by Eric5h5 on the above link
 
 
 public class Petrify : MonoBehaviour {
@@ -11,8 +13,6 @@ public class Petrify : MonoBehaviour {
     public GameObject playerL;
     public GameObject playerR;
     public TextMesh uiPiece;
-
-    public Animator animator;
 
     public float health=100;
     public float healthMod = 1;
@@ -33,8 +33,7 @@ public class Petrify : MonoBehaviour {
     void OnCollisionEnter(Collision col)
     {
 	petrifySound.Play(0);
-   animator.SetBool("jennyAttack", true);
-    petrifying = true;
+        petrifying = true;
     }
 
     void OnCollisionExit(Collision col)
