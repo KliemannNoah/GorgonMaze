33629fdb5dce744491993a31a564521cddcb9afc
NoahKliemann <kliemann@wisc.edu>
2018-11-28 08:49:58 -0600

    part4

---
+++ VR Room1v5/Assets/Opening.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Opening : MonoBehaviour {
+	
+	Animator doorOpenAnimation;
+	
+	private void OnTriggerEnter(Collider other){
+		doorOpenAnimation.SetTrigger("Door_Open"); 
+	}
+
+}
